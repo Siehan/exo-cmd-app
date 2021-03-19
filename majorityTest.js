e76904ebcb6bdf5457dd128ecd57fa43c0808358
Siehan <memain.ye.sylvie@gmail.com>
2021-03-19 07:17:07 +0100

    done

--- majorityTest.js
+++ majorityTest.js
@@ -11,3 +11,33 @@ Si l'âge est supérieur ou égal à 18 lui afficher:
 prenom nom, vous êtes majeur, vous pouvez voter
 Utiliser readline-sync pour l'interaction avec l'utilisateur
 */
+
+const chalk = require("chalk");
+const readlineSync = require("readline-sync");
+
+const userName = readlineSync.question("May I have your name?");
+const userFirstName = readlineSync.question("May I have your firstname?");
+const userAge = readlineSync.question("What is your age?");
+
+if (userAge <= 18) {
+  console.log(chalk.red(`Sorry ${userFirstName} ${userName} you are minor you can not vote`));
+} else {
+  console.log(chalk.green(`${userFirstName} ${userName} you are major you can vote`));
+}
+
+// node majorityTest.js
+
+/*
+OUTPUT exemple 1 :
+May I have your name? : Mémain-Yé
+May I have your firstname? Sylvie
+What is your age? 57
+Sylvie Mémain-Yé you are major you can vote (en couleur verte)
+*
+/*
+OUTPUT exemple 2 :
+May I have your name? Yé
+May I have your firstname? Mathis
+What is your age? 13
+Sorry Mathis Yé you are minor you can not vote (en couleur rouge)
+*/
--- sayMyName.js
+++ sayMyName.js
@@ -7,3 +7,8 @@ une string et affiche un message comme ci-dessous:
 % node sayMyName.js Sofiane
 My name is Sofiane.
 */
+
+const sayMyName = process.argv[2];
+console.log(`My name is ${sayMyName}`);
+// node sayMyName.js Sylvie
+// Ouput : My name is Sylvie
