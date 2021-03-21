b1ec7c4ca71da4376a3b1d985948c68252ac303d
Siehan <memain.ye.sylvie@gmail.com>
2021-03-21 19:33:52 +0100

    redone

--- majorityTest.js
+++ majorityTest.js
@@ -14,30 +14,57 @@ Utiliser readline-sync pour l'interaction avec l'utilisateur
 
 const chalk = require("chalk");
 const readlineSync = require("readline-sync");
+const process = require("process");
 
-const userName = readlineSync.question("Hello, may I have your family name?");
-const userFirstName = readlineSync.question("May I have your firstname?");
-const userAge = readlineSync.question("What is your age?");
+const userFirstName = readlineSync.question("Hello, may I have your firstname? ");
+const userName = readlineSync.question("And your family name? ");
+const userAge = readlineSync.question("What is your age? ");
+const age = Number(userAge);
 
-if (userAge < 18) {
+if (isNaN(userAge)) {
+  console.log(chalk.blue(`Sorry ${userFirstName}: you have to write a number, do it again !`));
+  //process.exit(1);
+  readlineSync.question("What is your age? ").userAge;
+  Number(userAge);
+}
+
+if (age < 18) {
   console.log(chalk.red(`Sorry ${userFirstName} ${userName} but you are minor and you can not vote.`));
 } else {
   console.log(chalk.green(`${userFirstName} ${userName} you are major and you can vote.`));
 }
 
 /*
-node majorityTest.js
 
 OUTPUT exemple 1 :
-Hello, may I have your family name? Mémain-Yé
-May I have your firstname? Sylvie
-What is your age? 12
+
+exo-cmd-app git:(main) ✗ node majorityTest.js
+Hello, may I have your firstname? Sylvie
+And your family name? Mémain-Yé
+What is your age? 17
 Sorry Sylvie Mémain-Yé but you are minor and you can not vote. (en couleur rouge)
-*
-/*
+
+
 OUTPUT exemple 2 :
-Hello, may I have your family name? Mémain-Yé
-May I have your firstname? Sylvie
+
+exo-cmd-app git:(main) ✗ node majorityTest.js
+Hello, may I have your firstname? Sylvie
+And your family name? Mémain-Yé
 What is your age? 57
 Sylvie Mémain-Yé you are major and you can vote. (en couleur verte)
+
+OUTPUT exemple 3 :
+
+Je voulais proposer à l'utilisateur de rentrer une nouvelle fois son âge
+avec des chiffres mais je n'ai pas réussi pour l'instant)
+
+  exo-cmd-app git:(main) ✗ node majorityTest.js
+Hello, may I have your firstname? Sylvie
+And your family name? Mémain-Yé
+What is your age? dix
+Sorry Sylvie: you have to write a number, do it again !
+What is your age? 10
+Sylvie Mémain-Yé you are major and you can vote.
+➜  exo-cmd-app git:(main) ✗
+
 */
