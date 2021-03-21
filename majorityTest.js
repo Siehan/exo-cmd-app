7a1b57a6f0878d46e2ed21b124ad780fd198a6d2
Siehan <memain.ye.sylvie@gmail.com>
2021-03-21 22:41:50 +0100

    redone

--- majorityTest.js
+++ majorityTest.js
@@ -16,25 +16,68 @@ const chalk = require("chalk");
 const readlineSync = require("readline-sync");
 const process = require("process");
 
-const userFirstName = readlineSync.question("Hello, may I have your firstname? ");
-const userName = readlineSync.question("And your family name? ");
-const userAge = readlineSync.question("What is your age? ");
-const age = Number(userAge);
+const MAJORITY = 18;
 
-if (isNaN(userAge)) {
-  console.log(chalk.blue(`Sorry ${userFirstName}: you have to write a number, do it again !`));
-  //process.exit(1);
-  readlineSync.question("What is your age? ").userAge;
-  Number(userAge);
+// Vérifier la ligne de commande
+if (process.argv.length !== 3) {
+  console.log("usage: node majorityTest.js age");
+  process.exit(1);
 }
 
-if (age < 18) {
-  console.log(chalk.red(`Sorry ${userFirstName} ${userName} but you are minor and you can not vote.`));
-} else {
-  console.log(chalk.green(`${userFirstName} ${userName} you are major and you can vote.`));
+if (isNaN(process.argv[2])) {
+  console.log(`Error: ${process.argv[2]} is not a number.`);
+  process.exit(1);
+}
+
+while (true) {
+  const userFirstName = readlineSync.question("Hello, may I have your firstname? : ");
+  const userName = readlineSync.question("And your family name? : ");
+  const userAge = readlineSync.question("What is your age? : ");
+
+  if (isNaN(userAge)) {
+    console.log(chalk.blue(`Sorry ${userFirstName}: "${userAge}" is not a number, do it again!`));
+    process.exit(1);
+    //readlineSync.question("What is your age? ").userAge
+  }
+
+  const age = Number(userAge);
+
+  if (age < MAJORITY) {
+    console.log(chalk.red(`Sorry ${userFirstName} ${userName} but you are minor and you can not vote.`));
+  } else {
+    console.log(chalk.green(`${userFirstName} ${userName} you are major and you can vote.`));
+  }
 }
 
 /*
+OUTPUT :
+
+➜  exo-cmd-app git:(main) ✗ node majorityTest.js
+usage: node majorityTest.js age
+➜  exo-cmd-app git:(main) ✗ node majorityTest.js 24
+Hello, may I have your firstname? : Sylvie
+And your family name? : Mémain-Yé
+What is your age? : 57
+Sylvie Mémain-Yé you are major and you can vote. (en vert)
+
+Hello, may I have your firstname? : Sylvie
+And your family name? : Mémain-Yé
+What is your age? : 16
+Sorry Sylvie Mémain-Yé but you are minor and you can not vote. (en rouge)
+
+Hello, may I have your firstname? : Sylvie
+And your family name? : Mémain-Yé
+What is your age? : dix
+Sorry Sylvie: "dix" is not a number, do it again! (en bleu)
+
+➜  exo-cmd-app git:(main) ✗ node majorityTest.js 17
+Hello, may I have your firstname? : Sylvie
+And your family name? : Mémain-Yé
+What is your age? : 18
+Sylvie Mémain-Yé you are major and you can vote. (en vert)
+
+
+
 
 OUTPUT exemple 1 :
 
@@ -42,7 +85,7 @@ exo-cmd-app git:(main) ✗ node majorityTest.js
 Hello, may I have your firstname? Sylvie
 And your family name? Mémain-Yé
 What is your age? 17
-Sorry Sylvie Mémain-Yé but you are minor and you can not vote. (en couleur rouge)
+Sorry Sylvie Mémain-Yé but you are minor and you can not vote. (couleur rouge)
 
 
 OUTPUT exemple 2 :
@@ -51,18 +94,18 @@ exo-cmd-app git:(main) ✗ node majorityTest.js
 Hello, may I have your firstname? Sylvie
 And your family name? Mémain-Yé
 What is your age? 57
-Sylvie Mémain-Yé you are major and you can vote. (en couleur verte)
+Sylvie Mémain-Yé you are major and you can vote. (couleur verte)
 
 OUTPUT exemple 3 :
 
 Je voulais proposer à l'utilisateur de rentrer une nouvelle fois son âge
-avec des chiffres mais je n'ai pas réussi pour l'instant)
+avec des chiffres mais je n'ai pas réussi pour l'instant.
 
   exo-cmd-app git:(main) ✗ node majorityTest.js
 Hello, may I have your firstname? Sylvie
 And your family name? Mémain-Yé
 What is your age? dix
-Sorry Sylvie: you have to write a number, do it again !
+Sorry Sylvie: "dix" is not a number, do it again! (couleur bleue)
 What is your age? 10
 Sylvie Mémain-Yé you are major and you can vote.
 ➜  exo-cmd-app git:(main) ✗
