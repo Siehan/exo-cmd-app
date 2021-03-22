a105eb15a5097890279cc1ec929544b98a5ee6b9
Siehan <memain.ye.sylvie@gmail.com>
2021-03-22 03:20:36 +0100

    done

--- guessNumber.js
+++ guessNumber.js
@@ -8,3 +8,51 @@ si le nombre entré par l'utilisateur est plus grand que le nombre secret: 'Nomb
 si le nombre entré par l'utilisateur est égal au nombre secret: 'Bravo!' en vert
 Utiliser readline-sync pour l'interaction avec l'utilisateur.
 */
+
+const chalk = require("chalk");
+const readlineSync = require("readline-sync");
+const process = require("process");
+
+const SECRETNUMBER = 10;
+
+// Vérifier qu'il n'y a qu'un seul argument passé au programme
+
+if (process.argv.length !== 3) {
+  console.log("usage: node guessNumber.js number");
+  process.exit(1);
+}
+
+// Vérifier que l'argument passé au programme peut être converti en nombre
+
+if (isNaN(process.argv[2])) {
+  console.log(`Error: ${process.argv[2]} is not a number.`);
+  process.exit(1);
+}
+
+while (true) {
+  const number = readlineSync.question("Rentre un nombre entre 1 et 10 : ");
+  if (isNaN(number)) {
+    console.log(chalk.blue(`Sorry: "${number}" is not a number, do it again!`));
+    process.exit(1);
+  }
+  if (number < SECRETNUMBER) {
+    console.log(chalk.red("Nombre trop petit"));
+  } else if (number > SECRETNUMBER) {
+    console.log(chalk.red("Nombre trop grand"));
+  } else {
+    number === SECRETNUMBER;
+    console.log(chalk.green("Bravo!"));
+    process.exit(0);
+  }
+}
+
+/*
+OUTPUT =
+➜  exo-cmd-app git:(main) ✗ node guessNumber.js 2
+Rentre un nombre entre 1 et 10 : 8
+Nombre trop petit
+Rentre un nombre entre 1 et 10 : 9
+Nombre trop petit
+Rentre un nombre entre 1 et 10 : 10
+Bravo!
+*/
