cb18e28f4baab244dc1fd5c64d4f34865a9d8b02
Siehan <memain.ye.sylvie@gmail.com>
2021-03-22 01:44:55 +0100

    update

--- sayMyName.js
+++ sayMyName.js
@@ -8,7 +8,13 @@ une string et affiche un message comme ci-dessous:
 My name is Sofiane.
 */
 
+if (process.argv.length != 3) {
+  console.log(`usage: node sayMyName.js name`);
+  process.exit(1); // exit the program with code status 1
+}
+
 const sayMyName = process.argv[2];
 console.log(`My name is ${sayMyName}`);
+
 // node sayMyName.js Sylvie
 // Ouput : My name is Sylvie
