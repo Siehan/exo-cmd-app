6ae2f1f06580163d4402cada6ccbf1890a2ed657
Siehan <memain.ye.sylvie@gmail.com>
2021-03-24 22:37:05 +0100

    re redone

--- majorityTest.js
+++ majorityTest.js
@@ -14,69 +14,59 @@ Utiliser readline-sync pour l'interaction avec l'utilisateur
 
 const chalk = require("chalk");
 const readlineSync = require("readline-sync");
-const process = require("process");
 
-const MAJORITY = 18;
-
-// Vérifier qu'il n'y a qu'un seul argument passé au programme
-
+// check command line
 if (process.argv.length !== 3) {
   console.log("usage: node majorityTest.js age");
   process.exit(1);
 }
 
-// Vérifier que l'argument passé au programme peut être converti en nombre
-
+// verify that the argument passed to the program can be converted to a number
 if (isNaN(process.argv[2])) {
   console.log(`Error: ${process.argv[2]} is not a number.`);
   process.exit(1);
 }
 
-while (true) {
-  const userFirstName = readlineSync.question("Hello, may I have your firstname? : ");
-  const userName = readlineSync.question("And your family name? : ");
-  const userAge = readlineSync.question("What is your age? : ");
+const MAJORITY = Number(process.argv[2]);
 
-  if (isNaN(userAge)) {
-    console.log(chalk.blue(`Sorry ${userFirstName}: "${userAge}" is not a number, do it again!`));
-    process.exit(1);
+while (true) {
+  let userFirstName = "";
+  let userLastName = "";
+  let userAge = "";
+
+  // get the first name and check if it's completed or not
+  while (userFirstName.length === 0) {
+    userFirstName = readlineSync.question("Hello, may I have your first name? : ");
+    if (userFirstName.length === 0) {
+      console.log(chalk.yellow("Oups! You have to write your first name"));
+      //Sans le while on peut inscrire continue OU process.exit(1) pour sortir du programme;
+    }
+  }
+  // get the Last name and check if it's completed
+  while (userLastName.length === 0) {
+    userLastName = readlineSync.question("And your family name? : ");
+    if (userLastName.length === 0) {
+      console.log(chalk.yellow("Oups! You have to write your Last name"));
+    }
+  }
+  // get age and check if it's a number
+  while (userAge.length === 0 || isNaN(userAge)) {
+    userAge = readlineSync.question("What is your age? : ");
+    if (userAge.length === 0) {
+      console.log(chalk.yellow(`Oups! ${userFirstName}, enter your age !`));
+      continue;
+    }
+    if (isNaN(userAge)) {
+      console.log(chalk.blue(`Sorry ${userFirstName}: "${userAge}" is not a number, try again!`));
+      continue;
+    }
   }
 
-  const age = Number(userAge);
+  age = Number(userAge);
 
   if (age < MAJORITY) {
-    console.log(chalk.red(`Sorry ${userFirstName} ${userName} but you are minor and you can not vote.`));
+    console.log(chalk.red(`Sorry ${userFirstName} ${userLastName} but you are minor and you can not vote.`));
   } else {
-    console.log(chalk.green(`${userFirstName} ${userName} you are major and you can vote.`));
+    console.log(chalk.green(`${userFirstName} ${userLastName} you are major so you can vote.`));
   }
 }
-
-/*
-
-OUTPUT :
-
-➜  exo-cmd-app git:(main) ✗ node majorityTest.js
-usage: node majorityTest.js age
-➜  exo-cmd-app git:(main) ✗ node majorityTest.js 24
-Hello, may I have your firstname? : Sylvie
-And your family name? : Mémain-Yé
-What is your age? : 57
-Sylvie Mémain-Yé you are major and you can vote. (en vert)
-
-Hello, may I have your firstname? : Sylvie
-And your family name? : Mémain-Yé
-What is your age? : 16
-Sorry Sylvie Mémain-Yé but you are minor and you can not vote. (en rouge)
-
-Hello, may I have your firstname? : Sylvie
-And your family name? : Mémain-Yé
-What is your age? : dix
-Sorry Sylvie: "dix" is not a number, do it again! (en bleu)
-
-➜  exo-cmd-app git:(main) ✗ node majorityTest.js 17
-Hello, may I have your firstname? : Sylvie
-And your family name? : Mémain-Yé
-What is your age? : 18
-Sylvie Mémain-Yé you are major and you can vote. (en vert)
-
-*/
